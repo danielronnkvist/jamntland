b6a6af7a8f0c4309c687b8739495e3194f6da272
danielronnkvist <daniel@ronnkvist.com>
2015-03-14 12:07:16 +0100

    Getting medelinkomst

--- scripts/controllers/HomeCtrl.js
+++ scripts/controllers/HomeCtrl.js
@@ -1,3 +1,7 @@
-kontApp.controller('HomeCtrl', ['$scope', function($scope){
-  $scope.message = 'hejsan';
+kontApp.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('data/medelinkomst.json').success(function(response){
+    $scope.data = response;
+  }).error(function(response){
+    console.error(response);
+  });
 }]);
